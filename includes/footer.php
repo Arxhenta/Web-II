9e467151b1c7dd75a9cbef6cbd8abf78b1ac3824
ardonagjyrevci <116449309+ardonagjyrevci@users.noreply.github.com>
2025-04-20 14:48:55 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,36 @@
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-4">
+                    <h3>Shkarko aplikacionin</h3>
+                    <h3>Shkarko aplikacionin për Android dhe iOS.</h3>
+                    <div class="app-logo">
+                        <img src="images/playstore.png">
+                        <img src="images/appstore.png">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <a href="index.php"><img src="images/logo.png" alt="A-Tech Logo"></a>
+                    <p>Ma mirë e lirë , veq lodhesh!</p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Informata</h3>
+                    <ul>
+                        <li><a href="terms.php">Termat dhe kushtet</a></li>
+                        <li><a href="returns.php">Politikat e kthimit</a></li>                  
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Kontakti </h3>
+                    <ul>
+                        <li>044-111-222</li>
+                        <li><a href="mailto:ardonagjyrevci42@gmail.com">atech@gmail.com</a></li>
 
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">Copyright 2025</p>
+        </div>
+    </div>
+</div>
