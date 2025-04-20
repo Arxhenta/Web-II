d84b4663f66e70141f7db4e59b791f31d3b49327
ardonagjyrevci <116449309+ardonagjyrevci@users.noreply.github.com>
2025-04-20 14:48:20 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>A-TECH | Ecommerce Website Design</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@496&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<?php
+session_start();
+?>
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.png" width="125px" alt="A-Tech Logo"></a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="products.php">Produktet</a></li>
+                        <li><a href="login.php">Kyçu</a></li>
+                    </ul>
+                </nav>
+                <a href="cart.php"><img src="images/cart.png" width="30px" height="30px"></a>
+            </div>
+        </div>
+    </div>
+</body>
 
