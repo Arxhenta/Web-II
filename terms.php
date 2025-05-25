af9f2854630c5f9323fdcec34fd0c976190d6dd3
Rona <rona.maxhuni@student.uni-pr.edu>
2025-05-25 21:30:13 +0200

    Create terms.php

---
+++ terms.php
@@ -0,0 +1,37 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="small-conatiner">
+    <br>
+    <br>
+    <br>
+    <h2 class="title">Termat dhe Kushtet</h2>
+    <big>
+    <div class="row">
+    <div class="col-3">
+            <h3>1. Informacion i Përgjithshëm</h3>
+            <p>Emri i biznesit: A-Tech Sh.P.K.<br>
+            Adresa: Magjistralia Prishtinë – Fushë Kosovë<br>
+            Email: atech@gmail.com<br>
+            Numri i telefonit: 044-111-222</p>
+<br><br>
+            <h3>2. Kushtet e Përdorimit të Faqes</h3>
+            <p>2.1. Përdorimi i përgjegjshëm: Ju jeni të detyruar të përdorni këtë faqe në përputhje me ligjet dhe rregullat në fuqi.</p>
+            <p>2.2. Regjistrimi i llogarisë: Për të kryer blerje, mund të jetë e nevojshme të krijoni një llogari.</p>
+    </div>
+    <div class="col-3">
+            <h3>3. Blerjet dhe Pagesat</h3>
+            <p>3.1. Çmimet: Të gjitha çmimet përfshijnë TVSH-në.<br>
+            3.2. Metodat e pagesës: Pranojmë pagesa me para në dorë.</p>
+        <br><br>
+            <h3>4. Politika e Kthimeve</h3>
+            <p>4.1. Kthimi i produkteve: E drejta e kthimit brenda 48 orëve.<br>
+            4.2. Kushtet: Produkti duhet të jetë i papërdorur dhe në paketimin origjinal.</p>
+    </div> 
+    </div>
+    </big>
+<br>
+<br>
+<br>
+</div>
+
+<?php include 'includes/footer.php'; ?>
