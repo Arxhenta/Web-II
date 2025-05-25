bcac66ea9a0b8ecfb5fe903e77a2fc8c62bff60f
ArxhenteKadriu <149197219+Arxhenta@users.noreply.github.com>
2025-05-25 21:25:32 +0200

    Create returns.php

---
+++ returns.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container"> 
+    <br><br>
+           <h2 class="title">Politikat e Kthimit dhe Rimbursimit</h2>
+   
+                <h3>Hyrje</h3>
+                <p style="font-size: 16px" !important;>Në A-Tech, ne kemi për qëllim të sigurojmë kënaqësi të plotë të klientit. Nëse nuk jeni të kënaqur me blerjen tuaj, kjo politikë shpjegon si mund të kthehet një produkt dhe si ne i trajtojmë rimbursimet.</p>
+            </div>
+<br>
+            <div class="row">
+                <div class="col-2">
+                <p style="font-size: 16px" !important><b>Politika e Kthimit</b></p>
+                <p style="font-size: 16px" !important>• Ju keni të drejtë të ktheni produktin brenda 24 orëve nga data e pranimit.<br>
+                • Produktet duhet të kthehen në gjendjen origjinale.<br>
+                • Disa artikuj mund të jenë të përjashtuar nga kthimi.</p>
+                <br>
+                <p style="font-size: 16px" !important><b>Procesi i Kthimit</b></p>
+                <p style="font-size: 16px" !important>• Kontaktoni atech@gmail.com për numrin e autorizimit.<br>
+                • Klientët mbulojnë kostot e transportit të kthimit.</p>
+                <br>
+                <p style="font-size: 16px" !important><b>Rimbursimi</b></p>
+                <p style="font-size: 16px" !important>• Rimbursimi bëhet përmes metodës origjinale të pagesës.<br>
+                • Procesimi bëhet brenda 3 ditë pune.</p>
+                </div>
+            </div>
+            <br><br>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
