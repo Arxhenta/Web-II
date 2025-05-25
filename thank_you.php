017b6cb3b344bd02d9c7198d49de3b5e581312ca
Rona <rona.maxhuni@student.uni-pr.edu>
2025-05-25 21:30:56 +0200

    Create thank_you.php

---
+++ thank_you.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php'; ?>
+<div class="small-container">
+    <br><br><br>
+    <h2 class="title">Faleminderit shumë që na keni zgjedhur!</h2>
+    <p style="font-size: 20px !important;">Porosia juaj ështe kryer dhe pranuar me sukses.<strong>Porosia arrin brenda 48 orëve!</strong></p>
+    <br><br><br>
+</div>
+<div class="row">
+    <a href="index.php" class="btn">Kthehu tek faqja kryesore</a>
+</div>
+<?php include 'includes/footer.php'; ?>
